7509a2835941140c68927f10f3e26ca7e4d3595a
Sam Milender <samuel.milender@kearsarge.org>
2012-12-13 09:26:22 -0500

    Added a collide for walls, and tried to do collide for other, but broke it.

--- Enemies.py
+++ Enemies.py
@@ -32,10 +32,30 @@ class Enemy():
     
     def collide(self, other):
         print "trying to hit other", screenHeight
+    if (self.rect.right > other.rect.left          
+        and self.rect.left < other.rect.right):
+            
+        if (self.rect.bottom > other.rect.top and 
+            self.rect.top < other.rect.bottom):
+                
+            if (self.distToPoint(other.rect.center)
+                < self.radius + other.radius): 
+                     
+                self.speed[0] = self.speed[0] * -1
+                self.speed[1] = self.speed[1] * -1
+                other.speed[0] = other.speed[0] * -1
+                other.speed[1] = other.speed[1] * -1
     
     def collideWall(self):
         print "trying to hit edges of screen", self.screenWidth, self.screenHeight
-        
+        if (self.rect.left < 0 
+            or self.rect.right > self.screenWidth):
+            self.speed[0] = self.speed[0]*-1
+        if (self.rect.top < 0 
+            or self.rect.bottom >self.screenHeight):
+            self.speed[1] = self.speed[1]*-1
+   
+
     def attack(self, other):
         print "trying to hit other", str(other)
         #if pygame.mixer:
