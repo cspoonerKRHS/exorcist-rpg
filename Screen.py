2543126218d735ce9d4f702cd4df8872f067dc07
Zander Blasingame <Zander.Blasingame@kearsarge.org>
2012-12-17 08:45:07 -0500

    taylor

---
+++ Screen.py
@@ -0,0 +1,76 @@
+import pygame, math, time
+
+class Screen():
+
+    # Methods or Functions
+    def __init__(self, images, speed, screenSize, waitMax = 1000):
+        self.surfaces = []
+        for image in images:
+            self.surfaces += [pygame.image.load(image)]
+        self.frame = 0
+        self.waitCount = 0
+        self.waitMax = waitMax
+        self.maxFrame = len(self.surfaces)-1
+        self.surface = self.surfaces[self.frame]
+        self.rect = self.surface.get_rect()
+        self.speed = speed
+        self.radius = self.rect.width/2
+        self.screenWidth = screenSize[0]
+        self.screenHeight = screenSize[1]
+        self.attacked = False
+        self.select = False
+        self.ran = False
+        self.living = False
+        self.value = 0
+
+
+    def place(self, pt):
+        self.rect = self.rect.move(pt)
+
+        
+    def move(self):
+        self.rect = self.rect.move(self.speed)
+        if self.frame < self.maxFrame:
+            self.frame += 1
+        else: 
+            self.frame = 0
+        self.surface = self.surfaces[self.frame]
+            
+    def animate(self):
+        if self.frame < self.maxFrame:
+            self.frame += 1
+        else:
+            self.frame = 0
+            # self.living = False
+        self.surface = self.surfaces[self.frame]
+            
+
+    def update(self, other):
+        if self.frame < self.maxFrame and self.select == True:
+            self.frame += 1
+            other.frame += 1
+        if self.frame == self.maxFrame and self.select == True or other.frame == other.maxFrame and other.select == True:
+           other.frame = 0
+           self.frame = 0
+        self.surface = self.surfaces[self.frame]
+        other.surface = other.surfaces[other.frame]
+        
+    def reset(self):
+        self.frame = 0
+        self.value = 0
+
+
+    def distToPoint(self, pt):
+        x1 = self.rect.center[0]
+        x2 = pt[0]
+        y1 = self.rect.center[1]
+        y2 = pt[1]
+        return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
+        
+    def wallCollide(self):
+        if(self.rect.top > 0):
+            self.rect.topleft = [0, -2500]
+            self.living = False           
+            return True
+        return False    
+		
