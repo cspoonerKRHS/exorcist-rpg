7ac187879835ea93c1ef28ae951dddd3597531c9
Taylor deGroot <taylor.degrootploof@kearsarge.org>
2013-01-04 09:18:40 -0500

    fix

--- Block.py
+++ Block.py
@@ -2,11 +2,10 @@ import pygame, math, sys
 
 class Block():
     def __init__(self, pos, screenSize, image, size):
-        size = []
         self.font = pygame.font.Font(None, 60)
         self.value = 0
         self.surface = pygame.image.load(image)
-        self.surface = pygame.transform.scale(self.surface,(size))
+        self.surface = pygame.transform.scale(self.surface, size)
         self.rect = self.surface.get_rect(centerx=pos[0], centery=pos[1])
         self.screenWidth = screenSize[0]
         self.screenHeight = screenSize[1]
