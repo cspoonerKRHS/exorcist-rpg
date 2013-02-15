6a8892be99aa3a7320960b7a36c96dc8ca0a6f6b
Taylor deGroot <taylor.degrootploof@kearsarge.org>
2013-02-15 09:19:52 -0500

    lava

--- Block.py
+++ Block.py
@@ -29,6 +29,14 @@ class Block():
                 self.rect.top < other.rect.bottom): 
                     other.speed[0] = other.speed[0] = 0
                     other.speed[1] = other.speed[1] = 0
+    
+    def lavaCollide(self, other):
+        if (self.rect.right > other.rect.left 
+            and self.rect.left < other.rect.right):
+            if (self.rect.bottom > other.rect.top and 
+                self.rect.top < other.rect.bottom): 
+                    other.speed[0] = other.speed[0] *.8
+                    other.speed[1] = other.speed[1] *.8
                     
     def deathplayerCollide(self, other, effect):
         other.nodamage = False
