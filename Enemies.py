90cc4e8595eff3d3564bab2db54ecb8f8b5f1b2a
Sam Milender <samuel.milender@kearsarge.org>
2013-02-19 08:31:29 -0500

    dunno

--- Enemies.py
+++ Enemies.py
@@ -10,12 +10,12 @@ class Enemy():
             "rcs/imgs/enemies/enemy.png"]
         for image in images:
             self.surfaces += [pygame.image.load(image)]
-        self.frontimgs = self.surfaces[0:2]
-        self.rightimgs = self.surfaces[3:5]
+        self.upimgs = self.surfaces[0:2]
+        self.downimgs = self.surfaces[3:5]
         self.leftimgs = self.surfaces[6:8]
-        self.backimgs = self.surfaces[9:11]
+        self.rightimgs = self.surfaces[9:11]
         self.attackimg = self.surfaces[12]
-        self.surfaces = self.frontimgs
+        self.surfaces = self.upimgs
         self.frame = 0
         self.maxFrame = len(self.surfaces) - 1
         self.surface = self.surfaces[self.frame]
@@ -29,6 +29,7 @@ class Enemy():
         self.living = True
         self.health = 10
         self.detectRadius = 100
+        self.dir = "up"
 
     def  __str__(self):
         pass
@@ -38,14 +39,14 @@ class Enemy():
         self.rect.center = pt
 #        print "I've moved to", pt
 
-    def dir(self, speed):
-        if speed[1] < 0:
+    def direction(self):
+        if self.speed[1] < 0:
             self.dir = "up"
-        elif speed[1] > 0:
+        elif self.speed[1] > 0:
             self.dir = "down"
-        elif speed[0] < 0:
+        elif self.speed[0] < 0:
             self.dir = "left"
-        elif speed[0] > 0:
+        elif self.speed[0] > 0:
             self.dir = "right"
 
     def move(self):
@@ -110,16 +111,32 @@ class Enemy():
         if self.distToPoint(player.rect.center) < self.detectRadius:
             if self.rect.center[0] < player.rect.center[0]:
                 self.speed[0] = 5
+                xdiff = math.abs(player.rect.center[0] - self.rect.center[0])
             if self.rect.center[0] > player.rect.center[0]:
                 self.speed[0] = -5
+                xdiff = math.abs(player.rect.center[0] - self.rect.center[0])
             if self.rect.center[1] < player.rect.center[1]:
                 self.speed[1] = 5
+                ydiff = math.abs(player.rect.center[1] - self.rect.center[1])
             if self.rect.center[1] > player.rect.center [1]:
                 self.speed[1] = -5
+                ydiff = math.abs(player.rect.center[1] - self.rect.center[1])
             if self.rect.center[0] == player.rect.center[0]:
                 self.speed[0] = 0
+                xdiff = 0
             if self.rect.center[1] == player.rect.center[1]:
                 self.speed[1] = 0
+                ydiff = 0
+        if ydiff > xdiff:       
+            if self.speed[1] < 0:
+                self.dir = "up"
+            elif self.speed[1] > 0:
+                self.dir = "down"
+        else:
+            elif self.speed[0] < 0:
+                self.dir = "left"
+            elif self.speed[0] > 0:
+                self.dir = "right"
 
     def enemyCollide(self, other):
         if (self.rect.right > other.rect.left
