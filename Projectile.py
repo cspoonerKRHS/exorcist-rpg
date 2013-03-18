a5172e41feac15c7c8f07afce7782f34adf1840a
Sam Milender <samuel.milender@kearsarge.org>
2013-03-18 09:20:54 -0400

    qaq

--- Projectile.py
+++ Projectile.py
@@ -25,27 +25,14 @@ class Projectile():
             self.frame += 1
         else:
             self.frame = 0
-    def animate2(self, other):        
-        if self.waitCount < 10:
-            self.waitCount += 1
-        if self.waitCount == 10:    
-            self.charge += 10
-            self.surface = pygame.transform.scale(
-                                self.baseSurfaces[self.frame], 
-                                (self.charge, self.charge))
-            if other.dir == "right" or other.dir == "stop right":
-                    self.rect.center = other.rect.midright
-            if other.dir == "left" or other.dir == "stop left":
-                self.rect.center = other.rect.midleft
-            self.waitCount = 0
-                
-    
+
     def distToPoint(self, pt):
         x1 = self.rect.center[0]
         x2 = pt[0]
         y1 = self.rect.center[1]
         y2 = pt[1]
         return math.sqrt(((x2-x1)**2)+((y2-y1)**2))
+
     def wallCollide(self):
         if (self.rect.left < 0 
             or self.rect.right > self.screenWidth):
@@ -53,7 +40,7 @@ class Projectile():
         if (self.rect.top < 0 
             or self.rect.bottom >self.screenHeight):
                 self.living = False
-    
+
     def otherCollide(self, other):
         if (self.rect.right > other.rect.left 
             and self.rect.left < other.rect.right):
@@ -61,5 +48,4 @@ class Projectile():
                 self.rect.top < other.rect.bottom): 
                 effect.upframe = True
                 effect.countframe = 1*dif
-                self.living = False
-                
+                self.living = False 
